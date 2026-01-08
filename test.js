211e14bd8e6d6c40999e3edd5a1270ed4f1f02f1
Javi <javi_zhang@hotmail.com>
2026-01-08 14:51:21 -0500

    file cleanup

--- test.js
+++
@@ -1,8 +0,0 @@
-function draw(){
-	console.log('one request from browser')
-	var canvas = document.getElementById('canv')
-	var ctx = canvas.getContext('2d')
-	var img = new Image()
-	img.onload = function(){ ctx.drawImage(img, 0, 0) }
-	img.src = './c/data3/ME49.png'
-}
